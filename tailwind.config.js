c6f274cb8d2decf0c8d10ee2a63889b62f6de5be
sarem eskandary <saremeskandary@gmail.com>
2022-02-21 02:30:32 +0330

    some customization

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,37 @@
 module.exports = {
   content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    screens: {
+      sm: '480px', //TODO delete it and if it worked ..
+      md: '768px',
+      lg: '976px',
+      xl: '1440px',
+    },
+    colors: {
+      'blue': '#1fb6ff',
+      'purple': '#7e5bef',
+      'pink': '#ff49db',
+      'orange': '#ff7849',
+      'green': '#13ce66',
+      'yellow': '#ffc82c',
+      'gray-dark': '#273444',
+      'gray': '#8492a6',
+      'gray-light': '#d3dce6',
+    },
+    fontFamily: {
+      sans: ['Graphik', 'sans-serif'], //TODO add font
+      serif: ['Merriweather', 'serif'],
+    },
+    extend: {    
+      backgroundImage: theme => ({
+        'header-image': "url('../public/assets/ELEMENTS-PC/bkgnd-header-PC.png')",
+        'main': "url('../public/assets/ELEMENTS-PC/bkgnd-main-PC.png')",
+      }),    
+    },
+
   },
   plugins: [],
 }
