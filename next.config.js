8f9e3d6d33e963bdc7976973af64227ec3147dc1
sarem eskandary <saremeskandary@gmail.com>
2022-02-21 20:48:32 +0330

    stake as defult page

--- next.config.js
+++ next.config.js
@@ -6,4 +6,13 @@
 module.exports = {
   reactStrictMode: true,
   swcMinify: true,
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/stake',
+        permanent: true,
+      },
+    ]
+  },
 };
