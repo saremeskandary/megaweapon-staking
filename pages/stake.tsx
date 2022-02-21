0f9092bd6f28c1fb635cd4b58f78d0b2f1d209e5
sarem eskandary <saremeskandary@gmail.com>
2022-02-21 23:35:44 +0330

    inute added

--- pages/stake.tsx
+++ pages/stake.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 
 function CardBackground({ children }) {
   return (
-    <div className="flex flex-row w-full justify-between border-2 border-night hover:opacity-100 px-2 py-3">
+    <div className="flex flex-row w-full justify-between border-2 border-night hover:opacity-100 px-3 py-5 rounded-md">
       {children}
     </div>
   );
@@ -29,28 +29,37 @@ export default function stake() {
     <Layout>
       <CardBackground>
         <input type="number" placeholder="stake amount" />
-        <div>
+        <div className="flex flex-row items-center gap-1 border-night border-2 px-4">
+          <div className="">Stake</div>
           <Image
-            src={`/../public/assets/icon-addstake-default.png`}
-            alt={"lock"}
+            src="/../public/assets/icon-addstake-default.png"
+            alt="lock"
             width="20"
             height="25"
           />
         </div>
-        <div>Stake</div>
       </CardBackground>
 
       <CardBackground>
-        <div>Set staking period</div>
+        <input type="number" placeholder="stake amount" />
+        <div className="flex flex-row items-center gap-1 border-night border-2 px-4">
+          <div className="">Set staking period</div>
+          <Image
+            src="/../public/assets/icon-addstake-default.png"
+            alt="lock"
+            width="20"
+            height="25"
+          />
+        </div>
       </CardBackground>
       <CardBackground>
-        <div className="flex flex-row">
-          <div className="m-2">
+        <div className="flex flex-col w-full">
+          <div className="flex flex-row justify-between w-full p-2">
             <div>Your balance</div>
             <div>3 $WEPON</div>
           </div>
-          <div>
-            <div className="m-2">Total duration</div>
+          <div className="flex flex-row justify-between w-full p-2">
+            <div>Total duration</div>
             <div>3 day, 2 hours, 5 minutes</div>
           </div>
         </div>
