d830952f948e55f7a7da1e449ec62853095b987e
sarem eskandary <saremeskandary@gmail.com>
2022-02-23 16:30:00 +0330

    change kind to dark

--- components/Card.tsx
+++ components/Card.tsx
@@ -1,12 +1,12 @@
 interface ICard {
   children: any;
-  kind: "dark"|"light";
+  dark?: boolean;
 }
-export function Card<ICard>({ children, kind = "light" }) {
+export function Card({ children, dark }:ICard) {
   return (
     <div
       className={`flex flex-row flex-wrap md:flex-nowrap gap-3 w-full justify-between border-2  hover:opacity-100 px-3 py-5 rounded-lg ${
-        kind === "dark"
+        dark
           ? "bg-cardbg-dark text-gray-100  border-gray-100"
           : "bg-cardbg-light text-gray-900  border-gray-900"
       }`}
