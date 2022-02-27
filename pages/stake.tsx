d07945c93109ca1b7a52f67525a764932bbecaea
smileman926 <smileman926@gmail.com>
2022-02-27 10:41:44 -0500

    small change for caution dialog style

--- pages/stake.tsx
+++ pages/stake.tsx
@@ -7,14 +7,14 @@ import { useState } from "react";
 
 function Caution({ onCancel }) {
   return (
-    <div className="flex flex-col p-2 gap-2 w-80 border-2 rounded-md border-red-800 bg-alarm">
+    <div className="flex flex-col py-2 px-4 mb-4 gap-2 w-80 border-2 rounded-md border-red-800 bg-alarm">
       <h2 className="text-xl self-center font-consolab px-2 rounded-md bg-cardbg-light text-red-600 ">
         Caution!
       </h2>
       <p className="text-lg font-consolaz rounded-md bg-cardbg-light p-2">
         You're about to lose money. Claim your ETH rewards before you unstake.
       </p>
-      <div className="flex flex-row self-end gap-2 ">
+      <div className="flex flex-row self-end gap-2">
         <button
           onClick={onCancel}
           className="border-b-4 text-base border-transparent hover:border-red-600 "
@@ -22,7 +22,7 @@ function Caution({ onCancel }) {
           Close
         </button>
         <Link href="/claim">
-          <a className="border-b-4 text-lg border-transparent hover:border-green-800">
+          <a className="border-b-4 text-base border-transparent hover:border-green-700">
             Claim
           </a>
         </Link>
