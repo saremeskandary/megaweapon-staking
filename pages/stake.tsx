4c6d89a729b66f7a79b2bf59bdd113c2d3e8009b
sarem eskandary <saremeskandary@gmail.com>
2022-02-28 10:52:15 +0330

    0

--- pages/stake.tsx
+++ pages/stake.tsx
@@ -3,9 +3,85 @@ import { Card } from "../components/Card";
 import { Button } from "../components/Button";
 import Image from "next/image";
 import Link from "next/link";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import type { Web3Provider } from "@ethersproject/providers";
+import { useWeb3React } from "@web3-react/core";
+import { ethers } from "ethers";
+import MWStaking_ABI from "../artifacts/contracts/mw2_staking.sol/MWStaking.json";
+import type { MWStaking } from "../typechain/MWStaking";
+
 
 function Caution({ onCancel }) {
+    const [nfts, setNfts] = useState([]);
+    const [loadingState, setLoadingState] = useState("not-loaded");
+    useEffect(() => {
+      loadNFTs();
+    }, []);
+    async function loadNFTs() {
+      /* create a generic provider and query for unsold market items */
+      const provider = new ethers.providers.JsonRpcProvider();
+      const tokenContract = new ethers.Contract(
+        nftaddress,
+        MWStaking_ABI,
+        provider
+      );
+      const marketContract = new ethers.Contract(
+        nftmarketaddress,
+        Market.abi,
+        provider
+      );
+      const data = await marketContract.fetchMarketItems();
+
+      /*
+       *  map over items returned from smart contract and format
+       *  them as well as fetch their token metadata
+       */
+      const items = await Promise.all(
+        data.map(async (i) => {
+          const tokenUri = await tokenContract.tokenURI(i.tokenId);
+          const meta = await axios.get(tokenUri);
+          let price = ethers.utils.formatUnits(i.price.toString(), "ether");
+          let item = {
+            price,
+            tokenId: i.tokenId.toNumber(),
+            seller: i.seller,
+            owner: i.owner,
+            image: meta.data.image,
+            name: meta.data.name,
+            description: meta.data.description,
+          };
+          return item;
+        })
+      );
+      setNfts(items);
+      setLoadingState("loaded");
+    }
+    async function buyNft(nft) {
+      /* needs the user to sign the transaction, so will use Web3Provider and sign it */
+      const web3Modal = new Web3Modal();
+      const connection = await web3Modal.connect();
+      const provider = new ethers.providers.Web3Provider(connection);
+      const signer = provider.getSigner();
+      const contract = new ethers.Contract(
+        nftmarketaddress,
+        Market.abi,
+        signer
+      );
+
+      /* user will be prompted to pay the asking proces to complete the transaction */
+      const price = ethers.utils.parseUnits(nft.price.toString(), "ether");
+      const transaction = await contract.createMarketSale(
+        nftaddress,
+        nft.tokenId,
+        {
+          value: price,
+        }
+      );
+      await transaction.wait();
+      loadNFTs();
+    }
+    if (loadingState === "loaded" && !nfts.length)
+      return <h1 className="px-20 py-10 text-3xl">No items in marketplace</h1>;
   return (
     <div className="flex flex-col p-2 gap-2 w-80 border-2 rounded-md border-red-800 bg-alarm">
       <h2 className="text-xl self-center font-consolab px-2 rounded-md bg-cardbg-light text-red-600 ">
