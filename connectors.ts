1557ba7b18489275e8dfda1f9c1a080eacdcfea0
sarem eskandary <saremeskandary@gmail.com>
2022-03-03 12:23:11 +0330

    port changed to 1337

--- connectors.ts
+++ connectors.ts
@@ -1,5 +1,5 @@
 import { InjectedConnector } from "@web3-react/injected-connector";
 
 export const injected = new InjectedConnector({
-  supportedChainIds: [31337], //TODO check available newtorks that Dapp deployed on them
+  supportedChainIds: [1337], //TODO check available newtorks that Dapp deployed on them
 });
