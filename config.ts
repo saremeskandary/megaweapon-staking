964edf907052e15d4a098937fc8d4941c007dba2
sarem eskandary <saremeskandary@gmail.com>
2022-03-04 21:58:32 +0330

    ropsten added

--- config.ts
+++ config.ts
@@ -1,2 +1,6 @@
-export const weaponAddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3"
-export const mwStakingAddress = "0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512"
+
+export const weaponAddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3";
+export const mwStakingAddress = "0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512";
+// ropsten 
+// export const weaponAddress = "0x2100669Cc69B9BFa9d630F0Cd6d1DcDDA4a06c9B"
+// export const mwStakingAddress = "0x37e08690dD10778BE8e1aB23516CF30194c1B9Bd"
