e9929c5a2a15a686679d958d9d64f92309579ebf
sarem eskandary <saremeskandary@gmail.com>
2022-03-06 23:06:48 +0330

    0

---
+++ components/CalculateTimeLeft.tsx
@@ -0,0 +1,45 @@
+import { useEffect, useState } from "react";
+
+export default function CalculateTimeLeft(a, b) {
+  function calculateTimeLeft(a) {
+    let difference = +a - +b;
+    let timeLeft = {};
+    if (difference > 0) {
+      timeLeft = {
+        days: Math.floor(difference / (1000 * 60 * 60 * 24)),
+        hours: Math.floor((difference / (1000 * 60 * 60)) % 24),
+        minutes: Math.floor((difference / 1000 / 60) % 60),
+        seconds: Math.floor((difference / 1000) % 60),
+      };
+    }
+    return timeLeft;
+  }
+  const [timeLeft, setTimeLeft] = useState(calculateTimeLeft(a));
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setTimeLeft(calculateTimeLeft(a));
+    }, 1000);
+
+    return () => clearTimeout(timer);
+  });
+
+  const timerComponents = [];
+
+  Object.keys(timeLeft).forEach((interval) => {
+    if (!timeLeft[interval]) {
+      return;
+    }
+
+    timerComponents.push(
+      <span>
+        {timeLeft[interval]} {interval}{" "}
+      </span>
+    );
+  });
+
+  return (
+    <div>
+      {timerComponents.length ? timerComponents : <span>Time's up!</span>}
+    </div>
+  );
+}
