0e41f72ae9e29056bde0a1dd5548616c6e378ab7
sarem eskandary <saremeskandary@gmail.com>
2022-03-08 03:05:43 +0330

    useSwr added

--- hooks/useGetStake.tsx
+++ hooks/useGetStake.tsx
@@ -3,24 +3,64 @@ import { useWeb3React } from "@web3-react/core";
 import { useEffect, useState } from "react";
 import { weaponAddress } from "../config";
 import { useWeaponContract } from "./useContract";
+import useSWR from "swr";
+import type { WEAPON } from "../typechain/WEAPON";
+import useKeepSWRDataLiveAsBlocksArrive from "./useKeepSWRDataLiveAsBlocksArrive";
 
-export default function useGetStake() {
-  const { account, active } = useWeb3React<Web3Provider>();
-  const [Stake, setStake] = useState([]);
-  const contract = useWeaponContract(weaponAddress);
 
-  async function getStake(account: string) {
-    const staked = await contract.getStake(account);
-    return staked;
-  }
+// export default function useGetStake() {
+//   const { account, active } = useWeb3React<Web3Provider>();
+//   const [Stake, setStake] = useState([]);
+//   const contract = useWeaponContract(weaponAddress);
 
-  useEffect(() => {
-    if (active) {
-      async () => {
-        // [stakedBalance, stakeBeginTime, stakeEndTime]
-        setStake(await getStake(account));
-      };
-    } else console.log("wallet its not active");
-  }, [active]);
-  return Stake;
+//   async function getStake(account: string) {
+//     const staked = await contract.getStake(account);
+//     return staked;
+//   }
+
+//   useEffect(() => {
+//     if (active) {
+//       async () => {
+//         // [stakedBalance, stakeBeginTime, stakeEndTime]
+//         setStake(await getStake(account));
+//       };
+//     } else console.log("wallet its not active yet");
+//   }, [active]);
+//   return Stake;
+// }
+
+
+
+
+function getStake(contract: WEAPON) {
+  return async (_: string, address: string) => {
+    const balance = await contract.getStake(address);
+
+    return balance;
+  };
+}
+
+export default function useGetStake(
+  address: string,
+  contractAddress: string,
+  suspense = false
+) {
+  const contract = useWeaponContract(contractAddress);
+
+  const shouldFetch =
+    typeof address === "string" &&
+    typeof contractAddress === "string" &&
+    !!contract;
+
+  const result = useSWR(
+    shouldFetch ? ["getStake", address, contractAddress] : null,
+    getStake(contract),
+    {
+      suspense,
+    }
+  );
+
+  useKeepSWRDataLiveAsBlocksArrive(result.mutate);
+
+  return result;
 }
