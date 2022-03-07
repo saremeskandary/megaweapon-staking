9054bb8579b87c67ee9c31d079f934cd53c26665
sarem eskandary <saremeskandary@gmail.com>
2022-03-08 03:25:37 +0330

    setPoolSize added

--- hooks/useSetPoolSize.tsx
+++ hooks/useSetPoolSize.tsx
@@ -1,5 +1,6 @@
 import { Web3Provider } from "@ethersproject/providers";
 import { useWeb3React } from "@web3-react/core";
+import { BigNumberish } from "ethers";
 import { useEffect, useState } from "react";
 import { mwStakingAddress } from "../config";
 import { useMW2StakingContract } from "./useContract";
@@ -7,16 +8,17 @@ import { useMW2StakingContract } from "./useContract";
 export default function useSetPoolSize() {
   const { active } = useWeb3React<Web3Provider>();
   const [click, setClick] = useState<boolean>(false);
+  const [poolSize, setPoolSize] = useState<BigNumberish>();
   const contract = useMW2StakingContract(mwStakingAddress);
 
   useEffect(() => {
     if (active && click) {
       let a = async () => {
-          await contract.setPoolSize(mwStakingAddress);
+        await contract.setPoolSize(poolSize);
       };
       a();
     }
     return setClick(false);
   }, [active, click]);
-  return { setClick };
+  return { setClick, setPoolSize };
 }
