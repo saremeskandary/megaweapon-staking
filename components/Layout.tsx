907899c941f5a7215095a2cb788f5b812873e1a2
sarem eskandary <saremeskandary@gmail.com>
2022-03-08 02:59:57 +0330

    wallet active chacking

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,21 +1,29 @@
 import Header from "./Header";
 import Image from "next/image";
+import { useWeb3React } from "@web3-react/core";
+import { Web3Provider } from "@ethersproject/providers";
 
 export default function Layout({ children }: any) {
+  const { active } = useWeb3React<Web3Provider>();
+
   return (
     <div className="bg-main w-screen h-screen bg-no-repeat bg-cover bg-center bg-fixed overflow-x-hidden font-consola text-2xl">
       <link rel="preload" href="/fonts/consola.ttf" as="font" crossOrigin="" />
       <link rel="preload" href="/fonts/consolab.ttf" as="font" crossOrigin="" />
       <link rel="preload" href="/fonts/consolai.ttf" as="font" crossOrigin="" />
       <link rel="preload" href="/fonts/consolaz.ttf" as="font" crossOrigin="" />
-
       <div className="flex flex-col items-center justify-center">
         <Header />
         <div className="flex flex-col sm:w-144 w-full mt-14 gap-2 items-center justify-center px-2 pb-4">
-          {children}
+          {active ? (
+            children
+          ) : (
+            <div className="bg-neutral-900  text-red-500 rounded-xl p-2 text-center">
+              you need to connect to your wallet
+            </div>
+          )}
         </div>
       </div>
-
       <div className="flex flex-row-reverse m-2">
         <Image
           src={`/assets/ELEMENTS/graphic/gfx-cautionlabel.png`}
@@ -25,6 +33,7 @@ export default function Layout({ children }: any) {
           className="h-full"
         />
       </div>
+      s
     </div>
   );
 }
