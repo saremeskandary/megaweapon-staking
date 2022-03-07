14544b53a46e9a6c2f9be4848babe9184f1f6eec
sarem eskandary <saremeskandary@gmail.com>
2022-03-08 02:57:28 +0330

    dinaminc data added need to be test.

--- hooks/useClaim.ts
+++ hooks/useClaim.ts
@@ -1,13 +1,27 @@
+import { Web3Provider } from "@ethersproject/providers";
+import { useWeb3React } from "@web3-react/core";
 import { BigNumberish } from "ethers";
+import { useEffect, useState } from "react";
+import { mwStakingAddress } from "../config";
+import { useMW2StakingContract } from "./useContract";
 import { MWStaking } from "../typechain/MWStaking";
 
-export const useClaim = async (
-  mwStaking: MWStaking,
-  epochs: BigNumberish[]
-) => {
-  mwStaking
-    ? await mwStaking.claim(epochs)
-    : console.log(
-        "you need to connect to your wallet and choose the correct network "
-      );
+export const useClaim = () => {
+  const mwStaking = useMW2StakingContract(mwStakingAddress);
+  const { active } = useWeb3React<Web3Provider>();
+  const [click, setClick] = useState(false);
+  const [epochs, setEpochs] = useState<BigNumberish[]>();
+
+  useEffect(() => {
+    if (active && click) {
+      const a = async () => {
+        await mwStaking.claim(epochs);
+      };
+      a();
+    }
+    return () => {
+      setClick(false);
+    };
+  }, [active, click]);
+  return { setClick, setEpochs };
 };
